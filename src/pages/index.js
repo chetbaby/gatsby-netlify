4be954e2c9afea2b01f3f10259368040083b64e8
Chet Hay <chet.hay@rosedigital.co>
2020-09-19 13:05:22 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>this stupid guy again</h1>
+    <p>will you die already</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: `1000px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
